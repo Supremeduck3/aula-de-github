05211a37602c1ae73ab02b1d7e5353e7f8098b4b
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-03-28 09:58:42 -0300

    fix: atualizei a sintaxe

---
+++ Operadores_logicos/maior_que_100.js
@@ -0,0 +1,9 @@
+let num1= 101
+let num2= 23
+let num3=4
+
+if (num1 || num2 || num3 > 100){
+    console.log("Passou de 100 ")
+}else{
+    console.log("Nenhum passou de 100")
+}
--- Operadores_logicos/pode_votar.js
+++ Operadores_logicos/pode_votar.js
@@ -1,7 +1,7 @@
 let idade = 59;
 
 if (idade >= 16 && idade <= 70){
-    console.log("pode votar");
+    console.log("Pode votar");
 }else{
     console.log("Não pode votar!");
 }
