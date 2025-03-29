0256eb73ebf694dce0a864a04074e89d52d9c782
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-28 23:51:41 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_and_Else/clima_do_dia.js
@@ -0,0 +1,9 @@
+let temperatura = 30;
+
+if (temperatura <= 15 ){
+    console.log("Está frio! ")
+}else if (temperatura > 15 && temperatura <= 25 ){
+    console.log("O clima está agradável! ");
+}else{
+    console.log("Está quente");
+}
