03191ee5db39bbb2910e1261be797217f490d4f9
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-29 00:46:23 -0300

    feat(switch-case): adiciona calculadora básica com operações

---
+++ Switch_case/calculadora.js
@@ -0,0 +1,16 @@
+let num1= 10;
+let num2= 40;
+let operador = "*"
+
+switch (operador){
+    case operador = "-": console.log(num1-num2);
+    break
+    case operador = "+": console.log(num1+num2);
+    break
+    case operador = "*": console.log(num1*num2);
+    break
+    case operador = "/": console.log(num1/num2);
+    break
+    default:console.log("Não é possível realizar a operação ");
+    break
+}
