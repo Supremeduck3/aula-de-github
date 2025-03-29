23589499cacdad58d718713665d812d43d472635
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-28 23:45:02 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ If_and_Else/Menor_3.js
@@ -0,0 +1,13 @@
+let num1 = 9;
+let num2 = 1;
+let num3 = 5;
+
+if (num1 < 3){
+    console.log("O primeiro número é menor que três ");
+}else if (num2 < 3){
+    console.log("O segundo número é menor que três ");
+}else if (num3 < 3){
+    console.log("O terceiro número é menor que três");
+}else{
+    console.log("Nenhum dos três números é maior que três");
+}
