12206cd73d669c339035b0d0a9e987e044ce92fb
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-28 22:23:21 -0300

    Fix: Alteração no codigo

--- Operadores_Aritimeticos/quadrado_cubo.js
+++ Operadores_Aritimeticos/quadrado_cubo.js
@@ -1,5 +1,5 @@
 let numero1 = 10;
-let quadrado = numero ** 2;
-let cubo = numero ** 3;
+let quadrado = numero1 ** 2;
+let cubo = numero1 ** 3;
 
 console.log('Seu número ao quadrado é ' + quadrado + ' e ao cubo é ' + cubo);
