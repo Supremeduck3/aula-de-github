c7f1bb2d8b3b4c77d9139c2197fd3ebfef801bdf
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-29 00:11:40 -0300

    feat(switch-case): adiciona exibição do dia da semana

---
+++ Switch_case/dia_da_semana.js
@@ -0,0 +1,20 @@
+let dia = 6;
+
+switch (dia){
+    case (dia = 2): console.log("Segunda-feira");
+    break
+    case (dia = 3):console.log("Terça-feira");
+    break
+    case (dia = 4):console.log("Quarta-feira");
+    break
+    case (dia = 5):console.log("Quinta-feira");
+    break
+    case (dia = 6):console.log("Sexta-feira");
+    break
+    case (dia = 7):console.log("Sábado");
+    break
+    case (dia = 1):console.log("Domingo");
+    break
+    default: console.log("Não é valido ");
+    break
+}
