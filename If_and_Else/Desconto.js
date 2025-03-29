781c4808c2cb06d9ff28161785579b4635ac0d04
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-28 23:09:41 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If_and_Else/Desconto.js
@@ -0,0 +1,10 @@
+let preço = 1000;
+
+if (preço > 2000){
+    let desconto = preço*10/100;
+    console.log("Seu sálario com o desconto de 10% é " + (preço-desconto));
+}else{
+    let Valor1 = preço*5/100;
+    console.log("Seu salario com o desconto de 5% é " + (preço-Valor1));
+
+}
