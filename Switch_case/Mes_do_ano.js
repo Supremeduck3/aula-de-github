43c7e634d28b54a03c46a63cdf7c6e0665effce4
Supremduck008 <pedro.otaviobraga@gmail.com>
2025-03-29 00:36:27 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch_case/Mes_do_ano.js
@@ -0,0 +1,30 @@
+let mês = 10;
+
+switch (mês){
+    case mês = 1 : console.log("Janeiro");
+    break
+    case mês = 2 : console.log("Fevereiro");
+    break
+    case mês = 3 : console.log("Março");
+    break
+    case mês = 4 : console.log("Abril");
+    break
+    case mês = 5 : console.log("Maio");
+    break
+    case mês = 6 : console.log("Junho");
+    break
+    case mês = 7 : console.log("Julho");
+    break
+    case mês = 8 : console.log("Agosto");
+    break
+    case mês = 9 : console.log("Setembro");
+    break
+    case mês = 10 : console.log("Outubro");
+    break
+    case mês = 11 : console.log("Novembro");
+    break
+    case mês = 12 : console.log("Dezembro");
+    break
+    default:console.log("Não é permitido");
+    break
+}
