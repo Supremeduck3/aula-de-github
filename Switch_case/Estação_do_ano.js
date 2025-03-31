a7e63d19aa78eb39ff1d35bc57669c9eb584515d
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-03-31 08:39:52 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_case/Estação_do_ano.js
@@ -0,0 +1,17 @@
+let estação = 4;
+
+switch(estação){
+    case 1: 
+    console.log("Verão");
+    case 2: 
+    console.log("Outono");
+    break
+    case 3: 
+    console.log("Inverno");
+    break
+    case 4: 
+    console.log("Primavera");
+    break
+    default:
+    console.log("Esse caractere não é valido coloque um valor de 1-4");
+}
