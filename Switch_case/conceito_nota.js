f3bfcb3a5638c11d227cd8dba631ab229866457c
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-03-31 08:30:35 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_case/conceito_nota.js
@@ -0,0 +1,27 @@
+let nota = 6
+
+
+switch(nota){
+    case 10:
+    case 9:
+        console.log("Excelente")
+        break
+    case 8:
+    case 7:
+        console.log("Muito bom")
+        break
+    case 6:
+    case 5:
+        console.log("Ok")
+        break
+    case 4:
+    case 3:
+        console.log("Ruim")
+        break
+    case 2:
+    case 1:
+    case 0:
+        console.log("Vai embora do curso!")
+        break
+    default:console.log("Esse caractere não é valido!")
+}
